534c25c0a27297cedc1a1da2e88b476672619bb7
Yuvraj-1411 <bathyuvrajsingh@gmail.com>
2024-04-18 14:03:29 +0530

    complete SignUp page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,50 @@
-import React from "react";
+// import React from "react";
+import { Link,useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-const SignUp = () => {
+
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError]= useState(null);
+  const [loading, setLoading]= useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      
+      setLoading(true);
+      const res = await fetch('/api/auth/signup',
+      {
+        method : 'POST',
+        headers : {
+          'Content-Type' : 'application/json', 
+        },
+        body : JSON.stringify(formData),
+      }
+    );
+    const data = await res.json();
+    if(data.success==false)
+    {
+      setError(data.message);
+      setLoading(false);
+      return;
+    }
+    setLoading(false);
+    setError(null);
+    navigate('/sign-in')
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="h-[100vh] items-center flex justify-center px-5 lg:px-0">
       <div className="max-w-screen-xl bg-white border shadow sm:rounded-lg flex justify-center flex-1">
@@ -23,51 +67,56 @@ const SignUp = () => {
               </p>
             </div>
             <div className="w-full flex-1 mt-8">
-              <div className="mx-auto max-w-xs flex flex-col gap-4">
-                <input
-                  className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                  type="text"
-                  placeholder="Username"
-                  id='username'
-                />
-                <input
-                  className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                  type="email"
-                  placeholder="Email"
-                  id="email"
-                />
-                <input
-                  className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                  type="password"
-                  placeholder="Password"
-                  id="password"
-                />
-                <button className="mt-5 tracking-wide font-semibold bg-blue-900 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                  <svg
-                    className="w-6 h-6 -ml-2"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                    <circle cx="8.5" cy="7" r="4" />
-                    <path d="M20 8v6M23 11h-6" />
-                  </svg>
-                  <span className="ml-3">Sign Up</span>
-                </button>
-                <p className="mt-6 text-xs text-gray-600 text-center">
-                  Already have an account?{" "}
-                  <a href="/sign-in">
-                    <span className="text-blue-900 font-semibold">Sign in</span>
-                  </a>
-                </p>
-              </div>
+              <form onSubmit={handleSubmit}>
+                <div className="mx-auto max-w-xs flex flex-col gap-4">
+                  <input
+                    className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="text"
+                    placeholder="Username"
+                    id="username"
+                    onChange={handleChange}
+                  />
+                  <input
+                    className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="email"
+                    placeholder="Email"
+                    id="email"
+                    onChange={handleChange}
+                  />
+                  <input
+                    className="w-full px-5 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="password"
+                    placeholder="Password"
+                    id="password"
+                    onChange={handleChange}
+                  />
+                  <button disabled={loading} className="mt-5 tracking-wide font-semibold bg-blue-900 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                    <svg
+                      className="w-6 h-6 -ml-2"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                    >
+                      <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                      <circle cx="8.5" cy="7" r="4" />
+                      <path d="M20 8v6M23 11h-6" />
+                    </svg>
+                    <span className="ml-3">{ loading ? 'Loading...':'Sign Up'}</span>
+                  </button>
+                  <p className="mt-6 text-xs text-gray-600 text-center">
+                    Already have an account?{" "}
+                    <Link to={'/sign-in'}>
+                      <span className="text-blue-900 font-semibold">
+                        Sign in
+                      </span>
+                    </Link>
+                  </p>
+                </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-};
-export default SignUp;
+}
